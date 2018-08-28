e1bbb5f364fcd47186324babaf88a5be15f0e195
Szymek <szymon.rzadca@gmail.com>
2018-08-28 18:30:15 +0200

    challenge two completed

--- DjangoChallengeOne/urls.py
+++ DjangoChallengeOne/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from ChallengeApp import views
+from django.conf.urls import include
+
 urlpatterns = [
-    url(r'^$', views.index, name="Index"),
+    url(r'^$', views.index),
+    url(r'^help/', include('ChallengeApp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
